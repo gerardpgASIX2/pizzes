bd67af22566596c71651083cd84e322efdefec1e
gerardpgASIX2 <455962.clot@fje.edu>
2025-02-04 17:40:41 +0000

    Commit 4 de l'aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -1,2 +1,32 @@
-Has demanat només la pizza bàsica. El preu és 9.68 € (IVA inclòs)<br>
-Autor: Gerard Perez Garcia
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>FES LA TEVA PIZZA</title>
+</head>
+<body>
+    <?php
+    $preu = 8;
+    $IVA = 21;
+
+    if (empty($_GET['ingr'])) {
+        $pvp = (($preu * (100 + $IVA)) / 100);
+        echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
+        exit(0);
+    } else {
+        $pvp = (($preu + count($_GET['ingr'])) * (100 + $IVA)) / 100;
+        echo "Has demanat la pizza bàsica amb:<br>";
+        foreach ($_GET['ingr'] as $ingredient) {
+            if ($ingredient) {
+                echo "* $ingredient<br>";
+            }
+        }
+        echo "El preu és $pvp € (IVA inclòs)<br>";
+        exit(0);
+    }
+
+    echo "<br>";
+    echo "Autor: dacomo<br>";
+    ?>
+</body>
+</html>
